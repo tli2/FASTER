f472e7276ed2a8c45c2c2999c1d01b956819080b
Tianyu Li <litianyu@mit.edu>
2021-02-10 15:46:32 -0500

    fixed scripts

--- cs/memcached-scripts/run-benchmark.py
+++ cs/memcached-scripts/run-benchmark.py
@@ -6,7 +6,8 @@ import socket
 import sys
 
 args = []
-cluster = ["10.0.1.27", "10.0.1.28"]
+cluster = ["10.0.1.27", "10.0.1.28", "10.0.1.29", "10.0.1.30", "10.0.1.43", "10.0.1.32", "10.0.1.33", "10.0.1.34",
+           "10.0.1.35", "10.0.1.36", "10.0.1.37", "10.0.1.38", "10.0.1.39", "10.0.1.40", "10.0.1.41", "10.0.1.42"]
 
 def serversForExpSize(size):
   result = ''
@@ -18,17 +19,14 @@ def serversForExpSize(size):
 class ExpHandler(socketserver.StreamRequestHandler):
   def handle(self):
     i = int(args[1])
-    data = self.rfile.readline().strip()
+    if (i < len(cluster) / 2):
+      return;
+    data = self.rfile.readline().decode().strip()
     experiment_size = int(data)
-    if (experiment_size = -1):
-      if (i >= len(cluster) / 2):
-        os.system('memaslap -s ' +  cluster[i - len(cluster) / 2] + ':11211 -T 16 -c 160 -F ~/memcached-scripts/memaslap.fill.cnf -x 250000000')
-    if (i >= experiment_size):
-      return 
-    if (i >= experiment_size / 2):
-      os.system('memaslap -s ' +  serversForExpSize(experiment_size) + '-T 16 -F ~/memcached-scripts/memaslap.run.cnf -d 1024 -t 30s'])
+    if (experiment_size == -1):
+      os.system('memaslap -s ' +  cluster[i - int(len(cluster) / 2)] + ':11211 -T 16 -c 160 -F ~/memcached-scripts/memaslap.fill.cnf -x 250000000')
     else:
-      return
+      os.system('memaslap -s ' +  serversForExpSize(experiment_size) + ' -T 16 -F ~/memcached-scripts/memaslap.run.cnf -d 1024 -t 30s')
 
 if __name__ == "__main__":
     args = sys.argv[1:]
@@ -37,20 +35,20 @@ if __name__ == "__main__":
       server = socketserver.TCPServer((host, 15000), ExpHandler)
       server.serve_forever()
     if args[0] == 'coordinator':
-      experiment_size = args[1]
+      experiment_size = int(args[1])
       socks = []
-      for i in range(int(experiment_size / 2), experiment_size):
+      for i in cluster:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((cluster[i], 15000))
+        s.connect((i, 15000))
         s.send("{}\n".format(experiment_size).encode())
         socks.append(s)
       for s in socks:
         s.recv(1)
-    if args[0] = 'setup':
+    if args[0] == 'setup':
       socks = []
-      for i in range(int(experiment_size/2)):
+      for i in cluster:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((cluster[i], 15000))
+        s.connect((i, 15000))
         s.send("-1\n".encode())
         socks.append(s)
       for s in socks:
